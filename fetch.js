fdcd6aa37bbe403287b30217a4bee6df0c05c89e
riesenfeld <aron.riesenfeld@gmail.com>
2021-11-08 22:06:04 -0800

    changed the json fetch paths so they actually work

--- fetch.js
+++ fetch.js
@@ -1,57 +1,54 @@
-
-var JSONToFetch = ["http://localhost/testing/la_mapping/Community_Boundaries_(CSA).js",
-    "http://localhost/testing/la_mapping/2010_Census_Data_by_Tract.js",
-    "http://localhost/testing/la_mapping/Parks_and_Gardens.js",
-    "http://localhost/testing/la_mapping/museums.json"];
+var JSONToFetch = [
+  "json/Community_Boundaries_(CSA).geojson",
+  "json/2010_Census_Data_by_Tract.json",
+  "json/museums.json",
+  "json/Parks_and_Gardens.geojson",
+]
 
 async function asyncFetch() {
+  let communityData = await fetch(JSONToFetch[0]).then(function (response) {
+    let promise = response.json().then(function (json) {
+      return json
+    })
+    return promise
+  })
+  let censusData = await fetch(JSONToFetch[1]).then(function (response) {
+    let promise = response.json().then(function (json) {
+      return json
+    })
+    return promise
+  })
+  let museumData = await fetch(JSONToFetch[2]).then(function (response) {
+    let promise = response.json().then(function (json) {
+      return json
+    })
+    return promise
+  })
+  let greenspaceData = await fetch(JSONToFetch[3]).then(function (response) {
+    let promise = response.json().then(function (json) {
+      return json
+    })
+    return promise
+  })
 
-    let communityData = await fetch('http://localhost/testing/la_mapping/Community_Boundaries_(CSA).json')
-        .then(function(response){
-            let promise = response.json().then(
-                function(json){
-                    return json;
-                })
-            return promise;
-        });
-    let censusData = await fetch('http://localhost/testing/la_mapping/2010_Census_Data_by_Tract.json')
-        .then(function(response){
-            let promise = response.json().then(
-                function(json){
-                    return json;
-                })
-            return promise;
-        });
-    let museumData = await fetch('http://localhost/testing/la_mapping/museums.json')
-        .then(function(response){
-            let promise = response.json().then(
-                function(json){
-                    return json;
-                })
-            return promise;
-        });
-    let greenspaceData = await fetch('http://localhost/testing/la_mapping/Parks_and_Gardens.json')
-        .then(function(response){
-            let promise = response.json().then(
-                function(json){
-                    return json;
-                })
-            return promise;
-        });
+  let result = { communityData, censusData, museumData, greenspaceData }
 
-    let result = {communityData, censusData, museumData, greenspaceData};
-
-    return result;
+  return result
 }
 
 //promisedData is itself a Promise and must be handled appropriately.
-let promisedData = asyncFetch();
-
+let promisedData = asyncFetch()
 
 //Unpack all of the promises from promisedData for easier usage later.
-var communityData = promisedData.then((d)=>{return d.communityData});
-var censusData = promisedData.then((d)=>{return d.censusData});
-var museumData = promisedData.then((d)=>{return d.museumData});
-var greenspaceData = promisedData.then((d)=>{return d.greenspaceData});
-
-
+var communityData = promisedData.then((d) => {
+  return d.communityData
+})
+var censusData = promisedData.then((d) => {
+  return d.censusData
+})
+var museumData = promisedData.then((d) => {
+  return d.museumData
+})
+var greenspaceData = promisedData.then((d) => {
+  return d.greenspaceData
+})
